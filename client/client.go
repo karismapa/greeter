f272c877967f4087ecf88e5df7a2d4ecc8b527d4
karismapa <karisma.pratama@manyoption.co.id>
2021-01-04 12:55:52 +0700

    call streaming function on client

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/karismapa/greeter/greetpb"
@@ -11,7 +12,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Client main is running...")
+	fmt.Println("Client is running...")
 
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
@@ -20,6 +21,13 @@ func main() {
 	defer conn.Close()
 
 	c := greetpb.NewGreetServiceClient(conn)
+
+	unary(c)
+	serverStream(c)
+}
+
+func unary(c greetpb.GreetServiceClient) {
+	fmt.Println("Invoke Greet function...")
 
 	ctx := context.Background()
 	req := &greetpb.GreetRequest{
@@ -34,4 +42,31 @@ func main() {
 		log.Fatalf("Error while calling Greet: %v", err)
 	}
 	log.Printf("Response from Greet: %v", res.Result)
+}
+
+func serverStream(c greetpb.GreetServiceClient) {
+	fmt.Println("Invoke GetManyTimes function...")
+
+	ctx := context.Background()
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Karisma",
+			LastName:  "Pratama",
+		},
+	}
+	stream, err := c.GreetManyTimes(ctx, req)
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes: %v", err)
+	}
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			// reached end of stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading stream: %v", err)
+		}
+		log.Printf("Response from GreetManyTimes: %v", msg.GetResult())
+	}
 }
